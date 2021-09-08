f37dc68a211a25e909bcdb4648693444ead02261
Roman Korotaev <kro1986@ukr.net>
2021-09-08 16:12:11 +0300

    Запрос на бекенд вінесен в отдельній класс, дописана функция прокрутки scrollIntoView()

--- index.js
+++ index.js
@@ -1,58 +1,48 @@
 //import './css/common.css';
 import './src/css/common.css'
 import articleTpl from './src/templates/articleTpl.hbs'
+import refs from './src/js/refs'
+import ArticlesApiService from './src/js/api_service'
 
 
-const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    searchInput:  document.getElementById ('searchQuery'),
-    articlesConteiner: document.querySelector('.js-articles-container')
-}
-
-let queryItem = ''
-
-//  console.log(' searchInput :',  refs.searchInput.value);
+const articlesApiService = new ArticlesApiService;
 
 
 
+refs.searchForm.addEventListener ('submit', submitSearchForm)
 
+function submitSearchForm(e) {  
+    e.preventDefault();
 
-// Делаем запрос на бекенд и получаем массив с данными
-function getArticles(queryItem) {
-const options = {
-    headers: {
-        Authorization: '0dc0d8bf7bcd4ecca326d964568a1608'
-    }
-    }
-    
-const url = `https://newsapi.org/v2/everything?q=${queryItem}&language=en&pageSize=9&page=1`
+   // refs.articlesConteiner.innerHTML = '';
+    clearPhotosGallary();
+    articlesApiService.resetPage();//при новом запросе начинаем поиск со страницы №1
+    articlesApiService.query = e.currentTarget.elements.query.value;
+    articlesApiService.fetchArticle().then (articles => articlesMarkUp (articles))
+ }
 
-    
-    fetch(url, options)
-        .then(response => response.json())
-        // .then(console.log)
-        .then(result => {
-            console.log(result.articles);
-            articlesMarkUp(result.articles)
-        })
-        .catch(error => console.log(error))
-}
-
-    // Передаём в эту функцию масси в с данными для их рендеринга
+     // Передаём в эту функцию массив с данными для их рендеринга
 function articlesMarkUp(arr) {
     refs.articlesConteiner.insertAdjacentHTML('beforeend', articleTpl(arr))
 }
 
 
 
+refs.searchMoreBtn.addEventListener('click', onSearchMoreBtn)
 
-
-refs.searchForm.addEventListener ('click', clickOnSearchForm)
-
-function clickOnSearchForm(e) {  
-    e.preventDefault();
-    console.log(' searchInput.value :', refs.searchInput.value);
-    console.log('  e.currentTarget.elements.query.value :',  e.currentTarget.elements.query.value);
-    queryItem = e.currentTarget.elements.query.value
-    getArticles(queryItem)
+function onSearchMoreBtn() {
+    articlesApiService.incerementPage();
+     articlesApiService.fetchArticle().then (articles => articlesMarkUp (articles)).then (() =>  scrollGallery() )
  }
+
+
+ function clearPhotosGallary() {
+    refs.articlesConteiner.innerHTML = '';
+ }
+
+function scrollGallery() {
+     refs. searchMoreBtn.scrollIntoView({
+     behavior: 'smooth',
+     block: 'end',
+  });
+}
