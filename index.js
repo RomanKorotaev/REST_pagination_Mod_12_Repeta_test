471dd553c980fb351b743742ca96659c6679a0cb
Roman Korotaev <kro1986@ukr.net>
2021-09-09 11:30:53 +0300

    Сделан отдельный класс с загрузкой кнопки loadMore

--- index.js
+++ index.js
@@ -3,22 +3,42 @@ import './src/css/common.css'
 import articleTpl from './src/templates/articleTpl.hbs'
 import refs from './src/js/refs'
 import ArticlesApiService from './src/js/api_service'
+import LoadMoreBtn from './src/js/components/load-more-btn.js'
 
 
 const articlesApiService = new ArticlesApiService;
 
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true
+} );
 
+// 
+loadMoreBtn.enable()
 
-refs.searchForm.addEventListener ('submit', submitSearchForm)
+console.log ('loadMoreBtn = ', loadMoreBtn )
+
+refs.searchForm.addEventListener('submit', submitSearchForm);
+ loadMoreBtn.refs.button.addEventListener('click', onSearchMoreBtn)
 
 function submitSearchForm(e) {  
     e.preventDefault();
 
    // refs.articlesConteiner.innerHTML = '';
     clearPhotosGallary();
+
+    loadMoreBtn.show();
+    loadMoreBtn.disable();
+
     articlesApiService.resetPage();//при новом запросе начинаем поиск со страницы №1
     articlesApiService.query = e.currentTarget.elements.query.value;
-    articlesApiService.fetchArticle().then (articles => articlesMarkUp (articles))
+    articlesApiService.fetchArticle().then(articles =>
+        {
+            articlesMarkUp(articles);
+            loadMoreBtn.enable();
+        })
+
+   
  }
 
      // Передаём в эту функцию массив с данными для их рендеринга
@@ -28,11 +48,18 @@ function articlesMarkUp(arr) {
 
 
 
-refs.searchMoreBtn.addEventListener('click', onSearchMoreBtn)
+// refs.searchMoreBtn.addEventListener('click', onSearchMoreBtn)
 
 function onSearchMoreBtn() {
+    loadMoreBtn.disable();
     articlesApiService.incerementPage();
-     articlesApiService.fetchArticle().then (articles => articlesMarkUp (articles)).then (() =>  scrollGallery() )
+    articlesApiService.fetchArticle()
+        .then(articles => articlesMarkUp(articles))
+        .then(() =>
+        {
+            scrollGallery();
+            loadMoreBtn.enable();
+        })
  }
 
 
@@ -42,8 +69,11 @@ function onSearchMoreBtn() {
 
  // функция прокрутки/склола экрана к кнопке "Загрузить ещё" при дополнительной загрузке карточек.
 function scrollGallery() {
-     refs. searchMoreBtn.scrollIntoView({
+    console.log (' Сработала функция  scrollGallery(); ')
+     //refs.searchMoreBtn.scrollIntoView({
+    loadMoreBtn.refs.button.scrollIntoView({
      behavior: 'smooth',
      block: 'end',
   });
 }
+
--- src/js/refs.js
+++ src/js/refs.js
@@ -3,7 +3,7 @@ const refs = {
     searchForm: document.querySelector('.js-search-form'),
     //searchInput:  document.getElementById ('searchQuery'),
     articlesConteiner: document.querySelector('.js-articles-container'),
-    searchMoreBtn: document.querySelector ('[data-action="load-more"]')
+   // searchMoreBtn: document.querySelector ('[data-action="load-more"]')
 
 }
 export default refs;
